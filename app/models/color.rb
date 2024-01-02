c8c66f12d102525f037c099a3b3d5eb486a6e2d7
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-01-03 06:43:25 +0900

    関連付け定義の修正

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,6 @@
 class Color < ApplicationRecord
   has_many :favorites, dependent: :destroy
-
+  has_many :users, through: :favorites
   validates :name, :kana, :code, presence: true
   validates :code, uniqueness: { scope: %i[name kana ] }
 end
