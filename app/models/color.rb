6be611ab1bfacfc3cf8d78bc938f771138ae1029
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-01-02 23:55:50 +0900

    お気に入り機能実装のために,User,Colorモデルに関連付けを定義

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,6 @@
 class Color < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+
   validates :name, :kana, :code, presence: true
   validates :code, uniqueness: { scope: %i[name kana ] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_31_184856) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_02_143144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_31_184856) do
     t.datetime "updated_at", null: false
     t.string "kana", null: false
     t.index ["name", "kana", "code"], name: "index_colors_on_name_and_kana_and_code", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "color_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["color_id"], name: "index_favorites_on_color_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_31_184856) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "colors"
+  add_foreign_key "favorites", "users"
 end
