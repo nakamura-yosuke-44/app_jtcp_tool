f2ba3140aa0c66edbf58577ffc805e2d87aa9b0d
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-01-05 23:55:17 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "top#index"
-  resources :colors, only: %i(index)
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,4 +9,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  
+  resources :colors do
+    collection do
+      get 'favorites'
+    end
+  end
+
+  resources :favorites, only: %i[create destroy]
 end
