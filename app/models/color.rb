b2843d6502e15889a11805342b901ffeab637341
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-01-06 00:46:18 +0900

    色に関連するお気に入り情報をユーザーidで検索して取得するためのメソッドを定義

--- app/models/color.rb
+++ app/models/color.rb
@@ -3,4 +3,8 @@ class Color < ApplicationRecord
   has_many :users, through: :favorites
   validates :name, :kana, :code, presence: true
   validates :code, uniqueness: { scope: %i[name kana ] }
+
+  def favorite_find(user)
+    self.favorites.find_by(user_id: user.id)
+  end
 end
