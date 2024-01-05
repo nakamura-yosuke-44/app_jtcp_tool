8d074a3f6a409520cb9c4f6dee2e7b57fbe8cd1f
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-01-06 01:12:31 +0900

    ブックマークに関するメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
-  has_many :colors, through: :favorites
+  has_many :favorite_colors, through: :favorites, source: :color
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def bookmark(board)
+    favorite_colors << board
+  end
+
+  def unbookmark(board)
+    favorite_colors.destroy(board)
+  end
+
+  def bookmark?(board)
+    favorite_colors.include?(board)
+  end
 end
