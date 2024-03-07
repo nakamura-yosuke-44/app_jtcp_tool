cb9782b08979bc32ac8df81623dc25d52c3f089c
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-07 23:57:19 +0900

    トップページに関するルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "top#index"
-  
+  root "top#top"
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
