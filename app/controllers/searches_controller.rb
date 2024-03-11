dedc965d6a535dc65cf981d769729e1945d74f24
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-11 23:56:38 +0900

    ログインしていない状態でも検索できるように修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,6 @@
 class SearchesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def search
     colors = Color.search(params[:keyword]).order(:kana)
     render json: colors
