bcb399f6cdcb8fd861dc345e975965483518d358
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-11 23:57:33 +0900

    Userモデルにname属性のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@
 class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_colors, through: :favorites, source: :color
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
