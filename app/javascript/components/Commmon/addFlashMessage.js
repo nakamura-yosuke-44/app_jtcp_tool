e67642b941f2c99ac292973faac1de015905e63d
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-13 22:18:21 +0900

    フラッシュメッセージが画面幅いっぱいに広がらない不具合を修正

--- app/javascript/components/Commmon/addFlashMessage.js
+++ app/javascript/components/Commmon/addFlashMessage.js
@@ -7,7 +7,7 @@ const addFlashMessage = (id, message) => {
   const node = document.getElementById(id);
   const newDiv = document.createElement('div');
   newDiv.id = 'flashMessage';
-  newDiv.className = 'bg-green-500 px-4 py-2 text-white mb-4 fixed top-16 left-0 right-0 z-50';
+  newDiv.className = 'bg-green-500 px-4 py-2 text-white mb-4 fixed top-16 inset-x-0 z-50';
   newDiv.textContent = message;
   node.appendChild(newDiv);
   setTimeout(removeFlashMessage, 500);
