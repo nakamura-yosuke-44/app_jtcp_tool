0e117c59f7266d0e987566ee58d1d349921706aa
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-25 18:09:27 +0900

    一時保存

---
+++ spec/controllers/users_contorller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Devise::SessionsController, type: :controller do
+  context 'ログイン' do
+    it 'トップ画面が表示されること' do
+      login
+      visit favorites_colors
+      expect(current_path).to eq(root_path)
+      expect(flash[:notice]).to have_content 'ログインしました'
+    end
+  end
+end
