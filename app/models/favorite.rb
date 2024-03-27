10f4461daf91a36c4b7e3ab589ad4d4dbed05b0e
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2024-03-27 23:49:52 +0900

    一意制約のコードが間違っていたので修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -23,5 +23,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :color
 
-  validates :color_id, uniqueness: {scopr: :user}
+  validates :user_id, uniqueness: { scope: :color_id }
 end
